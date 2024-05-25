e2d578c136bcda5ba63973d3b2260898914798c8
NeoJhonn <jhonny.azevedo@gmail.com>
2024-05-25 00:18:11 -0300

    Abstração inicial criada

---
+++ src/entities/ContaCorrente.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class ContaCorrente {
+
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void depositar(double valor) {
+
+    }
+
+    public void sacar(double valor) {
+
+    }
+
+    public void transferir(double valor) {
+
+    }
+
+}
---
+++ src/entities/ContaPoupanca.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class ContaPoupanca {
+
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void depositar(double valor) {
+
+    }
+
+    public void sacar(double valor) {
+
+    }
+
+    public void transferir(double valor) {
+
+    }
+}
